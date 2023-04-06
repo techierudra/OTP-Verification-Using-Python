77f454c2e19017838b82da1c94d1432e66fec138
RUDRA GARAI <103626421+techierudra@users.noreply.github.com>
2023-04-06 11:04:18 +0530

    initial commit

---
+++ Main.py
@@ -0,0 +1,25 @@
+import os
+import math
+import random
+import smtplib
+
+digits="0123456789"
+OTP=""
+for i in range(6):
+    OTP+=digits[math.floor(random.random()*10)]
+otp = OTP + " is your OTP"
+msg= otp
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("rudragaraionline@gmail.com", "qpegjugiaxdglpof")
+emailid = input("Enter your email: ")
+s.sendmail('&&&&&&&&&&&',emailid,msg)
+for i in range(0,3):
+ a = input("Enter Your OTP >>: ")
+ if a == OTP:
+    print("Verified")
+    break
+ else:
+    print("Please Check your OTP again")
+if a!=OTP:
+   print("Resent OTP again")
